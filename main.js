5198d6282fe02214afcc24e4c40de6322ad06cd0
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-07-19 18:13:03 +0300

    Done with the read toggle

--- main.js
+++ main.js
@@ -23,6 +23,37 @@ function toBoolean(string){
     return string === 'true'
 }
 
+const form=`<div class="right">
+                <form class="form" id="form">
+                <div class="title">
+                <label for="title">Title</label>
+                <input type="text" id="title" placeholder="Title" required autofocus>
+                </div>
+                <div class="author">
+                <label for="author">Author</label>
+                <input type="text" id="author" placeholder="author" required>
+                </div>
+                <div class="pages">
+                <label for="Pages">Pages</label>
+                <input type="number" id="pages" placeholder="Pages of the book" required>
+                </div>
+                <div class="read">
+                <label for="read">Read</label>
+                <select id="read">
+                    <option value="true">Read</option>
+                    <option value="false">Not Read</option>
+                </select>
+                </div>
+                <div class="form-btn">
+                <button type="submit" id="submit" class="button">
+                    ADD NEW BOOK
+                </button>
+                </div>
+                </form>
+            </div>
+            <div class="right" id="right"></div>
+            `
+
 
 const title=document.getElementById('title')
 const author=document.getElementById('author')
@@ -31,16 +62,35 @@ const read=document.getElementById('read')
 const submit=document.getElementById('submit')
 const formApp=document.getElementById('form')
 const right=document.getElementById('right')
+const btn=document.getElementById("btn")
+const app=document.getElementById('app')
+const main=document.getElementById('main')
+const left=document.querySelector(".left")
+const heading=document.querySelector(".heading")
 
+btn.addEventListener('click',()=>{
+    btn.remove()
+    main.classList.toggle('not-visible')
+    left.classList.toggle('not-visible')
+    app.classList.remove('ref')
+    heading.classList.remove('ref')
+    right.classList.add('add-bg')
 
-submit.addEventListener('click', (e)=>{
-    if (formApp.checkValidity()){
-        e.preventDefault()
-        addBookLibrary(title.value, author.value,pages.value, toBoolean(read.value))
-        formApp.reset()
-        functionalDOM()
-    }
+    title.focus()
+
 })
+
+
+    submit.addEventListener('click', (e)=>{
+        if (formApp.checkValidity()){
+            e.preventDefault()
+            addBookLibrary(title.value, author.value,pages.value, toBoolean(read.value))
+            formApp.reset()
+            functionalDOM()
+            // updateDOM()
+            title.focus()
+        }
+    })
 
 
 function updateDOM(){
@@ -61,13 +111,14 @@ function updateDOM(){
                 ${value.read?"the book has been read": "Not yet read"}
             </p>
             <button class="remove">Remove</button>
-            <button class="read ${value.read? 'read': 'unread'}">${value.read? 'Unread': 'I have read'}</button>
+            <button class="read ${value.read? '': 'unread'}">${value.read? 'Unread?': 'I have read'}</button>
         </div>
         `
         cards+=item        
     })
     right.innerHTML=cards
 }
+
 
 function functionalDOM(){
     updateDOM()
@@ -76,15 +127,28 @@ function functionalDOM(){
         btn.addEventListener('click', (e)=>{
             console.log(e.target.parentElement.dataset.id);
             myLibrary.splice(e.target.parentElement.dataset.id, e.target.parentElement.dataset.id === '0' ? 1 : e.target.parentElement.dataset.id)
-            console.log(e.target.parentElement.dataset.id === '0' ? 1 : e.target.parentElement.dataset.id);
+            // console.log(e.target.parentElement.dataset.id === '0' ? 1 : e.target.parentElement.dataset.id);
             // updateDOM()
-            document.querySelector(`[data-id="${e.target.parentElement.dataset.id}"]`).style.display="none"
-
+            // document.querySelector(`[data-id="${e.target.parentElement.dataset.id}"]`).style.display="none"
+            document.querySelector(`[data-id="${e.target.parentElement.dataset.id}"]`).remove()
         })
     })
 
+    
+    const readBtn=document.querySelectorAll(".read")
+    readBtn.forEach((btn)=>{
+        btn.addEventListener('click',(e)=>{
+            const id=parseInt(e.target.parentElement.dataset.id)
+            myLibrary[id].read=!myLibrary[id].read
+            
+            btn.classList.toggle('unread')
+            btn.innerHTML=`${myLibrary[id].read? 'Unread?': 'I have read'}`
+            // console.log(myLibrary[id].read);
+        })
+    })
 }
 
-function click(id){
-    console.log(id);
+
+function toggleRead() {
+    
 }
